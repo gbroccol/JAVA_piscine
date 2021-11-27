f8aa63647a8f100157b24167c97c3e671e781097
gbroccol <72576425+gbroccol@users.noreply.github.com>
2021-11-27 12:29:16 +0300

    Delete day09/ex00/Chat directory

--- day09/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/repositories/UsersRepository.java
+++
@@ -1,6 +0,0 @@
-package edu.school21.sockets.repositories;
-
-import edu.school21.sockets.models.User;
-
-public interface UsersRepository extends CrudRepository<User> {
-}
--- day09/ex00/Chat/SocketServer/src/main/java/edu/school21/sockets/services/UsersService.java
+++
@@ -1,7 +0,0 @@
-package edu.school21.sockets.services;
-
-import java.sql.SQLException;
-
-public interface UsersService {
-    void signUp(String username, String password) throws SQLException;
-}
